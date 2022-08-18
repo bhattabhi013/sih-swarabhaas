5db14f699235df89a41ff2e38e65d770fd9bfa6d
Abhishek <bhattabhi013@gmail.com>
2022-08-19 02:17:57 +0530

    new package for jitsi

--- lib/home/model/jitsee_meet.dart
+++ lib/home/model/jitsee_meet.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:jitsi_meet/feature_flag/feature_flag.dart';
-import 'package:jitsi_meet/jitsi_meet.dart';
+
+import 'package:jitsi_meet_wrapper/jitsi_meet_wrapper.dart';
 
 class JitseeMeet {
   final user = FirebaseAuth.instance.currentUser;
@@ -10,25 +10,29 @@ class JitseeMeet {
       required bool isAudio,
       required bool isVideo}) async {
     try {
-      Map<FeatureFlagEnum, bool> featureFlags = {
-        FeatureFlagEnum.WELCOME_PAGE_ENABLED: true,
-        FeatureFlagEnum.CLOSE_CAPTIONS_ENABLED: true,
-      };
       // FeatureFlag featureFlag = FeatureFlag();
       // featureFlag.welcomePageEnabled = true;
       // featureFlag.closeCaptionsEnabled = true;
       // featureFlag.resolution = FeatureFlagVideoResolution
       //     .MD_RESOLUTION; // Limit video resolution to 360p
+      var options = JitsiMeetingOptions(
+        roomNameOrUrl: room,
+        isAudioOnly: isAudio,
+        isVideoMuted: isVideo,
+        userDisplayName: user!.displayName,
+        userEmail: user!.email,
+        // featureFlags: featureFlags,
+      );
 
-      var options = JitsiMeetingOptions(room: room)
-        ..userDisplayName = user!.displayName
-        ..userEmail = user!.email
-        ..userAvatarURL = user!.photoURL
-        ..audioOnly = isAudio
-        ..videoMuted = isVideo
-        ..featureFlags.addAll(featureFlags);
+      // var options = JitsiMeetingOptions(room: room
+      //   ..userDisplayName : user!.displayName
+      //   ..userEmail = user!.email
+      //   ..userAvatarURL = user!.photoURL
+      //   ..audioOnly = isAudio
+      //   ..videoMuted = isVideo
+      //   ..featureFlags.addAll(featureFlags));
 
-      await JitsiMeet.joinMeeting(options);
+      await JitsiMeetWrapper.joinMeeting(options: options);
     } catch (error) {
       debugPrint("error: $error");
     }
